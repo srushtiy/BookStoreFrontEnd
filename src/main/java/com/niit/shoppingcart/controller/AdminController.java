b00d4dc48cc6038ae007391465bec24c24ad2f81
Srushti <srushtiy@yahoo.com>
2016-08-06 13:52:16 +0530

    First Commit

---
+++ src/main/java/com/niit/shoppingcart/controller/AdminController.java
@@ -0,0 +1,65 @@
+package com.niit.shoppingcart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.niit.bookstore.dao.CategoryDAO;
+import com.niit.bookstore.dao.ProductDAO;
+import com.niit.bookstore.dao.SupplierDAO;
+import com.niit.bookstore.model.Category;
+import com.niit.bookstore.model.Product;
+import com.niit.bookstore.model.Supplier;
+
+@Controller
+public class AdminController {
+	
+	@Autowired
+	private Product product;
+
+	@Autowired
+	private Supplier supplier;
+
+	@Autowired
+	private Category category;
+	
+	@Autowired
+	private SupplierDAO supplierDAO;
+	
+	@Autowired
+	private CategoryDAO categoryDAO;
+	
+	
+	@Autowired
+	private ProductDAO productDAO;
+
+	
+	@RequestMapping("/manageCategories")
+	public ModelAndView categories() {
+		ModelAndView mv = new ModelAndView("/home");
+		mv.addObject("category", category);
+		mv.addObject("isAdminClickedCategories", "true");
+		mv.addObject("categoryList", categoryDAO.list());
+		return mv;
+	}
+
+	@RequestMapping("/manageProducts")
+	public ModelAndView suppliers() {
+		ModelAndView mv = new ModelAndView("/home");
+		mv.addObject("product", product);
+		mv.addObject("isAdminClickedProducts", "true");
+		mv.addObject("productList", productDAO.productlist());
+		return mv;
+	}
+
+	@RequestMapping("/manageSuppliers")
+	public ModelAndView products() {
+		ModelAndView mv = new ModelAndView("/home");
+		mv.addObject("supplier", supplier);
+		mv.addObject("isAdminClickedSuppliers", "true");
+		mv.addObject("supplierList", supplierDAO.supplierlist());
+		return mv;
+	}
+
+}
---
+++ src/main/webapp/views/AdminHome.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<html>
+
+<body>
+	<div id="adminHome">
+		<a href="manageCategories">Manage Categories</a> &nbsp; &nbsp;
+		<a href="manageSuppliers">Manage Suppliers</a>  &nbsp; &nbsp; 
+		<a href="manageProducts">Manage Products</a> &nbsp; &nbsp;
+
+	</div>
+
+
+</body>
+</html>
